44e8ac43f11b3b648e85ee863add144ff5e1a93a
vmnbhuvani <ap186832@gmail.com>
2020-03-29 15:27:11 +0530

    11. Styling

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,16 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import styled from 'styled-components'
 
 function createAlert(){
   alert('hello. you can click me!');
 }
+const pStyle = {
+  color: 'red',
+  fontSize: '2em'
+}
+const Paragraph = styled.p`
+color: 'blue',
+fontSize: '3em'
+`;
 
 function App() {
   return (
     <div className="App">
      <Header info="This is header info"></Header>
-     <p>Main Containt</p>
+     <p style={pStyle}>Main Containt</p>
+     <Paragraph>New Styling </Paragraph>
      <Footer author="vmn bhuvani" myAlert={createAlert} ></Footer>
     </div>
   );
